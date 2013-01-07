b84af2b737edbd47a6b7602ef321c8aa899e0f0e
thecharlie <charlie@charlie-roberts.com>
2013-01-07 12:50:10 -0800

    working knob rotation

--- scripts/interface.js
+++ scripts/interface.js
@@ -114,8 +114,7 @@ var widgetDefaults = {
 }
 
 Interface.Widget = {
-  init : function( options ) {
-    
+  init : function( options ) {        
     Interface.extend( this, widgetDefaults);
     
     Interface.extend( this, options);
@@ -131,7 +130,21 @@ Interface.Widget = {
       this.background = options.colors[0];
       this.fill       = options.colors[1];
       this.stroke     = options.colors[2];                
-    }       
+    }
+  },
+  
+  setValue : function(value, doNotDraw) {
+    var r = this.max - this.min,
+        v = value;
+        
+    this.value = value;
+                
+    if(this.min !== 0 || this.max !== 1) {
+      v -= this.min;
+      this._value = v / r;
+    }
+    
+    if(!doNotDraw) this.draw();
   },
   
   hitTest : function(e) {
@@ -222,7 +235,8 @@ Interface.Slider.prototype = Interface.Widget;
 Interface.Crossfader = function() {
   Interface.extend(this, {
     crossfaderWidth: 30,
-
+    _value : .5,
+    
     draw : function() {
       this.ctx.fillStyle = this.background;
       this.ctx.fillRect( this.x, this.y, this.width, this.height );
@@ -317,3 +331,145 @@ Interface.Button = function() {
   .init( arguments[0] );
 };
 Interface.Button.prototype = Interface.Widget;
+
+Interface.Knob = function() {
+  Interface.extend(this, {
+    _value: 0,
+    mode : 'toggle',
+    radius: 30,
+    knobBuffer:15,
+    lastPosition: 0,
+    
+    draw : function() {
+      //this.canvasCtx.clearRect(0, 0, this.width,this.height);
+      this.ctx.strokeStyle = this.stroke;
+      this.ctx.lineWidth = 1.5;
+	
+    	this.ctx.fillStyle = this.background; // draw background of widget first
+    
+      var angle0 = Math.PI * .6;
+      var angle1 = Math.PI * .4;
+    
+      this.ctx.beginPath();
+      this.ctx.arc(this.x + this.radius, this.y + this.radius, this.radius - this.knobBuffer, angle0, angle1, false);
+      this.ctx.arc(this.x + this.radius, this.y + this.radius, (this.radius - this.knobBuffer) * .3 , angle1, angle0, true);		
+      this.ctx.closePath();
+      this.ctx.fill();
+    
+      this.ctx.stroke();
+      this.ctx.fillStyle = this.fill;	// now draw foreground...
+	
+      /*if(this.centerZero) {
+          var angle3 = Math.PI * 1.5;
+          var angle4 = Math.PI * (1.5 + (-1 + (this.rotationValue * 2)));
+        
+          this.ctx.beginPath();
+          this.ctx.arc(this.radius , this.radius, this.radius -  this.knobBuffer, angle3, angle4, (this.rotationValue < .5));
+          this.ctx.arc(this.radius , this.radius, (this.radius - this.knobBuffer) * 0.3,  angle4, angle3, (this.rotationValue > .5));
+          this.ctx.closePath();
+          if(this.rotationValue > .495 && this.rotationValue < .505) { // draw circle if centered?
+              this.ctx.beginPath();
+              this.ctx.arc(this.radius , this.radius, (this.radius -  this.knobBuffer) * .3, 0, Math.PI*2, true); 
+              this.ctx.closePath();
+          }
+          this.ctx.fill();
+      } else {*/
+        //Math.abs(.4 - (1 - this._value) * 1.6 * Math.PI)
+        // .6 + this_value * 1.8 * Math.PI;
+          if(!this.isInverted)  { 
+            var angle2 = Math.PI * .6 + this._value * 1.8  * Math.PI;
+            if(angle2 > 2 * Math.PI) angle2 -= 2 * Math.PI;
+          }else{
+            var angle2 = Math.PI * (0.4 - (1.8 * this._value));
+          }
+        
+          this.ctx.beginPath();
+  		//context.arc(centerX, centerY, radius, startingAngle, endingAngle, antiClockwise);
+          if(!this.isInverted) {
+              this.ctx.arc(this.x + this.radius, this.y + this.radius, this.radius - this.knobBuffer, angle0, angle2, false);
+              this.ctx.arc(this.x + this.radius, this.y + this.radius, (this.radius - this.knobBuffer) * .3, angle2, angle0, true);
+          } else {
+              this.ctx.arc(this.x + this.radius, this.y + this.radius, this.radius - this.knobBuffer, angle1, angle2 ,true);
+              this.ctx.arc(this.x + this.radius, this.y + this.radius, (this.radius - this.knobBuffer) * .3, angle2, angle1, false);
+          }
+          this.ctx.closePath();
+          this.ctx.fill();
+      //}*/
+    },
+    
+    changeValue : function( xOffset, yOffset ) {
+      if(this.hasFocus || !this.requiresFocus) {
+    	// TODO: accommodate !usesRotation and centeredRotation.
+        this.lastValue = this.value;
+        var pointTwo = .2 * 2 * Math.PI;
+        if(!this.usesRotation) {
+          if (this.lastPosition != -1) { 
+            this._value -= (yOffset - this.lastPosition) / (this.radius * 2);
+            //console.log(this._value);
+          }
+        }else{
+            var xdiff = (this.x + (this.radius)) - xOffset;
+            var ydiff = (this.y + (this.radius)) - yOffset;
+            var angle = Math.PI + Math.atan2(ydiff, xdiff);
+            this._value =  ((angle + (Math.PI * 1.5)) % (Math.PI * 2)) / (Math.PI * 2);
+        }
+        //console.log(this.rotationValue);
+        if (this._value > 1) this._value = 1;
+        if (this._value < 0) this._value = 0;
+    
+    	/*if(this.lastRotationValue == .95 && this.rotationValue <= .5 && !this.newTouch) {
+    		this.rotationValue = .95;
+    		return;
+    	}else if(this.lastRotationValue == .05 && this.rotationValue >= .5 && !this.newTouch) {
+    		this.rotationValue = .05;
+    		return;
+    	}*/
+    	this.lastRotationValue = this._value;
+      this.lastPosition = yOffset;
+      
+      var range  = this.max - this.min;
+      this.value = this.min + this._value * range;
+      
+      this.draw();
+    
+      //       if(this.lastValue != this.value) {
+      //           this.setValue(this.value);
+      //           this.lastValue = this.value;
+      // if(this.displayValue)
+      //   this.label.setValue(this.value.toFixed(3));
+      //       }
+    	//Math.round(number).toFixed(2);
+      }
+    },
+    
+    hitTest : function(e) {
+      if(e.x >= this.x && e.x < this.x + this.radius * 2) {
+      	if(e.y >= this.y && e.y < this.y  + this.radius * 2) {  
+      		return true;
+      	} 
+      }
+    
+      return false;
+    },
+    
+    mousedown : function(e) {
+      if(this.mode !== 'contact') {
+        this.lastPosition = e.y - this.y;
+        
+        this.changeValue( e.x - this.x, e.y - this.y ); 
+      }else{
+        this._value = 1;
+        this.draw();
+        var self = this;
+        setTimeout( function() { self._value = 0; self.draw(); }, 200);
+      }
+    },
+    mousemove : function(e) { this.changeValue( e.x - this.x, e.y - this.y ); },
+    mouseup   : function(e) {
+      if(this.mode === 'momentary')
+        this.changeValue( e.x - this.x, e.y - this.y ); 
+    },    
+  })
+  .init( arguments[0] );
+};
+Interface.Knob.prototype = Interface.Widget;
