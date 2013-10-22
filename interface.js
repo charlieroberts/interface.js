1045b29f9408ed4530c14475bccb1ffa99f3df37
charlieroberts <bigbadotis@gmail.com>
2013-10-22 10:27:26 -0700

    Update interface.js
    Fixed bug in XY widget for sending OSC

--- interface.js
+++ interface.js
@@ -1518,7 +1518,7 @@ Interface.XY = function() {
       var tt = '';
       this._values.length = 0;
       for(var i = 0; i < this.values.length; i++) {
-        tt += ss;
+        tt += 'ff';
         this._values.push( this.values[i].x );
         this._values.push( this.values[i].y );        
       }
